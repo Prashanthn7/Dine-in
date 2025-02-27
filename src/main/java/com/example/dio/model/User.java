617b4fd2613bed4fcb50f9b96bd6aa5dde12202f
Prashanthn7 <pn342604gmail.com>
2025-02-27 14:37:49 +0530

    Modifying repository, service and controller class

--- src/main/java/com/example/dio/model/User.java
+++ src/main/java/com/example/dio/model/User.java
@@ -1,22 +1,42 @@
 package com.example.dio.model;
 
+import com.example.dio.enums.UserRole;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="user_id")
     private long userId;
+
+    @Column(name="user_name")
     private String username;
+
+    @Column(name="user_email")
     private String email;
+
+    @Column(name="password")
     private String password;
-    private long phoneno;
+
+    @Column(name="phone_number")
+    private String phoneNo;
+
+    @Column(name="created_at")
     private LocalDate createdAt;
+
+    @Column(name = "last_modified_date")
     private LocalDate lastModifyDate;
-    private String userRole;
+
+    @Column(name ="user_role")
+    private UserRole userRole;
 
 }
